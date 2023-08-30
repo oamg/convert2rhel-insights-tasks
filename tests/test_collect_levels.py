ce7e7fcbb5890cd82abf483c1fb8c453155cc304
Andrea Waltlova <awaltlov@redhat.com>
2023-08-30 13:34:57 +0200

    Initialize repository with script and first version of tests
    Make targets for tests
    README
    Coverage, Pylint, Pytest, Pre-commit
    
    Signed-off-by: Andrea Waltlova <awaltlov@redhat.com>

---
+++ tests/test_collect_levels.py
@@ -0,0 +1,19 @@
+from main import (
+    collect_report_level,
+)
+
+SAMPLE_ACTION_RESULTS = {
+    "action1": {
+        "result": {"level": "ERROR"},
+        "messages": [{"level": "SUCCESS"}, {"level": "WARNING"}]
+    },
+    "action2": {
+        "result": {"level": "WARNING"},
+        "messages": [{"level": "SUCCESS"}, {"level": "ERROR"}]
+    },
+}
+
+def test_collect_report_level():
+    expected_output = ["ERROR", "ERROR", "WARNING", "WARNING", "SUCCESS", "SUCCESS"]
+    result = collect_report_level(SAMPLE_ACTION_RESULTS)
+    assert result == expected_output
---
+++ tests/test_generate_report_message.py
@@ -0,0 +1,9 @@
+from main import (
+    generate_report_message,
+    STATUS_CODE
+)
+
+def test_generate_report_message():
+    for status_code in STATUS_CODE:
+        message = generate_report_message(status_code)
+        assert isinstance(message, str)
