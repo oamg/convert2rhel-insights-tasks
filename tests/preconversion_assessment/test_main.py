2a53fcb267a9f54bdb1e309891482f9071ff5030
Rodolfo Olivieri <rolivier@redhat.com>
2023-09-12 10:56:09 -0300

    Update script and tests
    Updated the tests and script with the rest of comments from PR.
    
    Signed-off-by: Rodolfo Olivieri <rolivier@redhat.com>

--- tests/preconversion_assessment/test_main.py
+++ tests/preconversion_assessment/test_main.py
@@ -6,13 +6,12 @@ from scripts.preconversion_assessment_script import main, ProcessError
 
 
 # fmt: off
-@patch("__builtin__.open", new_callable=mock_open())
-@patch("json.load", side_effect=[{"actions": []}])
+@patch("scripts.preconversion_assessment_script.gather_json_report", side_effect=[{"actions": []}])
 @patch("scripts.preconversion_assessment_script.verify_required_files_are_present", side_effect=Mock())
 @patch("scripts.preconversion_assessment_script.setup_convert2rhel", side_effect=Mock())
 @patch("scripts.preconversion_assessment_script.install_convert2rhel", side_effect=Mock())
 @patch("scripts.preconversion_assessment_script.run_convert2rhel", side_effect=Mock())
-@patch("scripts.preconversion_assessment_script.collect_report_level", side_effect=Mock(return_value=["SUCCESS"]))
+@patch("scripts.preconversion_assessment_script.find_highest_report_level", side_effect=Mock(return_value=["SUCCESS"]))
 @patch("scripts.preconversion_assessment_script.gather_textual_report", side_effect=Mock(return_value=""))
 @patch("scripts.preconversion_assessment_script.generate_report_message", side_effect=Mock(return_value=""))
 @patch("scripts.preconversion_assessment_script.transform_raw_data", side_effect=Mock(return_value=""))
@@ -23,13 +22,12 @@ def test_main_success(
     mock_transform_raw_data,
     mock_generate_report_message,
     mock_gather_textual_report,
-    mock_collect_report_level,
+    mock_find_highest_report_level,
     mock_run_convert2rhel,
     mock_install_convert2rhel,
     mock_setup_convert2rhel,
     mock_verify_required_files_are_present,
-    mock_json_load,
-    mock_open_func,
+    mock_gather_json_report,
 ):
     main()
 
@@ -37,23 +35,22 @@ def test_main_success(
     assert mock_setup_convert2rhel.call_count == 1
     assert mock_install_convert2rhel.call_count == 1
     assert mock_run_convert2rhel.call_count == 1
-    assert mock_json_load.call_count == 1
-    assert mock_collect_report_level.call_count == 1
+    assert mock_gather_json_report.call_count == 1
+    assert mock_find_highest_report_level.call_count == 1
     assert mock_gather_textual_report.call_count == 1
     assert mock_generate_report_message.call_count == 1
     assert mock_cleanup.call_count == 1
-    assert mock_open_func.call_count == 1
     assert mock_transform_raw_data.call_count == 1
 
 
 # fmt: off
 @patch("__builtin__.open", new_callable=mock_open())
-@patch("json.load", side_effect=[{"action_results": []}])
+@patch("scripts.preconversion_assessment_script.gather_json_report", side_effect=[{"actions": []}])
 @patch("scripts.preconversion_assessment_script.verify_required_files_are_present", side_effect=Mock())
 @patch("scripts.preconversion_assessment_script.setup_convert2rhel", side_effect=Mock())
 @patch("scripts.preconversion_assessment_script.install_convert2rhel", side_effect=Mock())
 @patch("scripts.preconversion_assessment_script.run_convert2rhel", side_effect=ProcessError("Process error"))
-@patch("scripts.preconversion_assessment_script.collect_report_level", side_effect=Mock(return_value=["SUCCESS"]))
+@patch("scripts.preconversion_assessment_script.find_highest_report_level", side_effect=Mock(return_value=["SUCCESS"]))
 @patch("scripts.preconversion_assessment_script.gather_textual_report", side_effect=Mock(return_value=""))
 @patch("scripts.preconversion_assessment_script.generate_report_message", side_effect=Mock(return_value=""))
 @patch("scripts.preconversion_assessment_script.cleanup", side_effect=Mock())
@@ -62,12 +59,12 @@ def test_main_process_error(
     mock_cleanup,
     mock_generate_report_message,
     mock_gather_textual_report,
-    mock_collect_report_level,
+    mock_find_highest_report_level,
     mock_run_convert2rhel,
     mock_install_convert2rhel,
     mock_setup_convert2rhel,
     mock_verify_required_files_are_present,
-    mock_json_load,
+    mock_gather_json_report,
     mock_open_func,
 ):
     main()
@@ -76,8 +73,8 @@ def test_main_process_error(
     assert mock_setup_convert2rhel.call_count == 1
     assert mock_install_convert2rhel.call_count == 1
     assert mock_run_convert2rhel.call_count == 1
-    assert mock_json_load.call_count == 0
-    assert mock_collect_report_level.call_count == 0
+    assert mock_gather_json_report.call_count == 0
+    assert mock_find_highest_report_level.call_count == 0
     assert mock_gather_textual_report.call_count == 0
     assert mock_generate_report_message.call_count == 0
     assert mock_cleanup.call_count == 1
@@ -90,7 +87,7 @@ def test_main_process_error(
 @patch("scripts.preconversion_assessment_script.setup_convert2rhel", side_effect=Mock())
 @patch("scripts.preconversion_assessment_script.install_convert2rhel", side_effect=Mock())
 @patch("scripts.preconversion_assessment_script.run_convert2rhel", side_effect=Mock())
-@patch("scripts.preconversion_assessment_script.collect_report_level", side_effect=Mock(return_value=["SUCCESS"]))
+@patch("scripts.preconversion_assessment_script.find_highest_report_level", side_effect=Mock(return_value=["SUCCESS"]))
 @patch("scripts.preconversion_assessment_script.gather_textual_report", side_effect=Mock(return_value=""))
 @patch("scripts.preconversion_assessment_script.generate_report_message", side_effect=Mock(return_value=""))
 @patch("scripts.preconversion_assessment_script.cleanup", side_effect=Mock())
@@ -99,7 +96,7 @@ def test_main_general_exception(
     mock_cleanup,
     mock_generate_report_message,
     mock_gather_textual_report,
-    mock_collect_report_level,
+    mock_find_highest_report_level,
     mock_run_convert2rhel,
     mock_install_convert2rhel,
     mock_setup_convert2rhel,
@@ -111,7 +108,7 @@ def test_main_general_exception(
     assert mock_setup_convert2rhel.call_count == 1
     assert mock_install_convert2rhel.call_count == 1
     assert mock_run_convert2rhel.call_count == 1
-    assert mock_collect_report_level.call_count == 0
+    assert mock_find_highest_report_level.call_count == 0
     assert mock_gather_textual_report.call_count == 0
     assert mock_generate_report_message.call_count == 0
     assert mock_cleanup.call_count == 1
