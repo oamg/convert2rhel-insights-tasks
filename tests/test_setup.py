7df959f26b51827968b32a15a72432ceca1fe41e
Andrea Waltlová <awaltlov@redhat.com>
2024-02-14 18:45:55 +0100

    Improve error when downoad of required file fails (#60)
    Signed-off-by: Andrea Waltlova <awaltlov@redhat.com>

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,6 +1,9 @@
+import pytest
 from mock import Mock
+from urllib2 import URLError
 
-from scripts.c2r_script import setup_convert2rhel
+
+from scripts.c2r_script import setup_convert2rhel, ProcessError
 
 
 class MockRequiredFile(object):
@@ -22,6 +25,23 @@ def test_setup_calls_backup_and_create_for_every_file():
     required_files = [test_file]
 
     setup_convert2rhel(required_files)
+
+    test_file.backup.assert_called_once()
+    test_file.create_from_host_url_data.assert_called_once()
+
+
+def test_setup_urlerror():
+    # pylint: disable=too-many-arguments
+    mocked_required_file = Mock(spec=MockRequiredFile)
+    test_file = mocked_required_file("/mock/path/file.txt", "foo.bar")
+    test_file.create_from_host_url_data.side_effect = URLError("foo")
+    required_files = [test_file]
+
+    with pytest.raises(ProcessError) as error:
+        setup_convert2rhel(required_files)
+        assert "Download of required file from foo.bar failed with error: foo" in str(
+            error
+        )
 
     test_file.backup.assert_called_once()
     test_file.create_from_host_url_data.assert_called_once()
