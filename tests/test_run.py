99576e6b90c405a8fdeda156dcedd3bdb5c1328a
Andrea Waltlova <awaltlov@redhat.com>
2024-03-01 09:51:06 +0100

    Use all existing  RHC_WORKER variables for script
    Signed-off-by: Andrea Waltlova <awaltlov@redhat.com>

--- tests/test_run.py
+++ tests/test_run.py
@@ -5,7 +5,11 @@ from scripts.c2r_script import run_convert2rhel
 
 @patch("scripts.c2r_script.SCRIPT_TYPE", "CONVERSION")
 def test_run_convert2rhel_conversion():
-    mock_env = {"PATH": "/fake/path", "RHC_WORKER_CONVERT2RHEL_DISABLE_TELEMETRY": "1"}
+    mock_env = {
+        "PATH": "/fake/path",
+        "RHC_WORKER_CONVERT2RHEL_DISABLE_TELEMETRY": "1",
+        "RHC_WORKER_FOO": "1",
+    }
 
     with patch("os.environ", mock_env), patch(
         "scripts.c2r_script.run_subprocess", return_value=(b"", 0)
@@ -14,7 +18,11 @@ def test_run_convert2rhel_conversion():
 
     mock_popen.assert_called_once_with(
         ["/usr/bin/convert2rhel", "-y"],
-        env={"PATH": "/fake/path", "CONVERT2RHEL_DISABLE_TELEMETRY": "1"},
+        env={
+            "PATH": "/fake/path",
+            "CONVERT2RHEL_DISABLE_TELEMETRY": "1",
+            "FOO": "1",
+        },
     )
 
 
