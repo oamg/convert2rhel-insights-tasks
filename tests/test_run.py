c17db2aa3d61ef0aa757258252a904fc01526ef6
Rodolfo Olivieri <rolivier@redhat.com>
2024-04-09 10:59:16 -0300

    Add unit_test for custom variables

--- tests/test_run.py
+++ tests/test_run.py
@@ -3,6 +3,22 @@ from mock import patch
 from scripts.c2r_script import run_convert2rhel
 
 
+@patch("scripts.c2r_script.SCRIPT_TYPE", "ANALYSIS")
+@patch("scripts.c2r_script.IS_ANALYSIS", True)
+def test_run_convert2rhel_custom_variables():
+    mock_env = {"FOO": "BAR", "BAR": "BAZ", "RHC_WORKER_LALA": "LAND"}
+
+    with patch("os.environ", mock_env), patch(
+        "scripts.c2r_script.run_subprocess", return_value=(b"", 0)
+    ) as mock_popen:
+        run_convert2rhel()
+
+    mock_popen.assert_called_once_with(
+        ["/usr/bin/convert2rhel", "analyze", "-y"],
+        env={"FOO": "BAR", "BAR": "BAZ", "LALA": "LAND"},
+    )
+
+
 @patch("scripts.c2r_script.SCRIPT_TYPE", "CONVERSION")
 def test_run_convert2rhel_conversion():
     mock_env = {
