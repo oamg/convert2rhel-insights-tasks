a55cb4f5bcce9444428428cbd1bc2da1d03d30e7
Rodolfo Olivieri <rolivier@redhat.com>
2024-06-18 10:10:04 -0300

    Add script execution to pre-commit

--- tests/test_output_schema.py
+++ tests/test_output_schema.py
@@ -1,22 +1,38 @@
 import json
 import jsonschema
+import pytest
 from convert2rhel_insights_tasks.main import OutputCollector, STATUS_CODE
 
 
-def test_output_schema():
+@pytest.mark.parametrize(
+    ("schema",),
+    (
+        ("schemas/convert2rhel_insights_tasks_schema_1.0.json",),
+        ("schemas/convert2rhel_insights_tasks_schema_1.1.json",),
+    ),
+)
+def test_output_schema(schema):
     """Test that (pre)conversion report is wrapped in expected json"""
     for status in STATUS_CODE:
         output_collector = OutputCollector(status=status)
         empty_output = output_collector.to_dict()
 
-        with open("schemas/conversion_schema_1.0.json", "r") as schema:
-            schema_json = json.load(schema)
+        with open(schema, "r") as handler:
+            schema_json = json.load(handler)
+
         # If some difference between generated json and its schema invoke exception
         jsonschema.validate(instance=empty_output, schema=schema_json)
         assert not empty_output["alert"]
 
 
-def test_output_schema_entries_report():
+@pytest.mark.parametrize(
+    ("schema",),
+    (
+        ("schemas/convert2rhel_insights_tasks_schema_1.0.json",),
+        ("schemas/convert2rhel_insights_tasks_schema_1.1.json",),
+    ),
+)
+def test_output_schema_entries_report(schema):
     """
     Test that pre-conversion report is wrapped in expected json with entries
     key.
@@ -27,7 +43,7 @@ def test_output_schema_entries_report():
     output_collector.entries = {"hi": "world"}
     full_output = output_collector.to_dict()
 
-    with open("schemas/conversion_schema_1.0.json", "r") as schema:
-        schema_json = json.load(schema)
+    with open(schema, "r") as handler:
+        schema_json = json.load(handler)
     # If some difference between generated json and its schema invoke exception
     jsonschema.validate(instance=full_output, schema=schema_json)
